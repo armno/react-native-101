bf34a24b146af63edf1daf96dee62de14497ff20
Armno <monkeyarmno@gmail.com>
2015-07-22 15:17:27 +0700

    reindent

--- index.ios.js
+++ index.ios.js
@@ -6,12 +6,12 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ListView
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	ListView
 } = React;
 
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
@@ -23,119 +23,119 @@ class reactNativeOne extends React.Component {
 	 *
 	 * create `this.state.dataSource` to hold api response data in a ListView object
 	 */
-  constructor() {
-    super();
-    this.state = {
-   		loaded: false,
-   		dataSource: new ListView.DataSource({
-   			rowHasChanged: (row1, row2) => row1 !== row2
-   		})
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			loaded: false,
+			dataSource: new ListView.DataSource({
+				rowHasChanged: (row1, row2) => row1 !== row2
+			})
+		};
+	}
 
-  /**
-   * get data from api once the component has been loaded
-   */
-  componentDidMount() {
-    this.fetchData();
-  }
+	/**
+	 * get data from api once the component has been loaded
+	 */
+	componentDidMount() {
+		this.fetchData();
+	}
 
-  /**
-   * get data from api server and store it in `this.state.dataSource`
-   * also set `loaded` flag to be TRUE so we can test in `render()` method
-   * if the component has already got the data
-   */
-  fetchData() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-        	loaded: true,
-        	dataSource: this.state.dataSource.cloneWithRows(responseData.movies)
-        });
-      })
-      .done();
-  }
+	/**
+	 * get data from api server and store it in `this.state.dataSource`
+	 * also set `loaded` flag to be TRUE so we can test in `render()` method
+	 * if the component has already got the data
+	 */
+	fetchData() {
+		fetch(REQUEST_URL)
+			.then((response) => response.json())
+			.then((responseData) => {
+				this.setState({
+					loaded: true,
+					dataSource: this.state.dataSource.cloneWithRows(responseData.movies)
+				});
+			})
+			.done();
+	}
 
-  /**
-   * render loading view (shown before the component got data from api server)
-   */
-  renderLoadingView() {
-    return (
-      <View style={ styles.container }>
-        <Text>
-          Loading movies ...
-        </Text>
-      </View>
-    );
-  }
+	/**
+	 * render loading view (shown before the component got data from api server)
+	 */
+	renderLoadingView() {
+		return (
+			<View style={ styles.container }>
+				<Text>
+					Loading movies ...
+				</Text>
+			</View>
+		);
+	}
 
-  /**
-   * render a single view of movie element
-   * @param  {object} movie
-   */
-  renderMovie(movie) {
-    return (
-      <View style={ styles.container }>
-        <Image source={{ uri: movie.posters.thumbnail }}
-          style={ styles.thumbnail } />
+	/**
+	 * render a single view of movie element
+	 * @param  {object} movie
+	 */
+	renderMovie(movie) {
+		return (
+			<View style={ styles.container }>
+				<Image source={{ uri: movie.posters.thumbnail }}
+					style={ styles.thumbnail } />
 
-        <View style={ styles.rightContainer }>
-          <Text style={ styles.title }>{ movie.title }</Text>
-          <Text style={ styles.year }>{ movie.year }</Text>
-        </View>
-      </View>
-    );
-  }
+				<View style={ styles.rightContainer }>
+					<Text style={ styles.title }>{ movie.title }</Text>
+					<Text style={ styles.year }>{ movie.year }</Text>
+				</View>
+			</View>
+		);
+	}
 
-  /**
-   * render the component in ListView
-   */
-  render() {
+	/**
+	 * render the component in ListView
+	 */
+	render() {
 
-    if ( !this.state.loaded ) {
-      return this.renderLoadingView();
-    }
+		if ( !this.state.loaded ) {
+			return this.renderLoadingView();
+		}
 
-    return (
-    	<ListView
-    		dataSource={ this.state.dataSource }
-    		renderRow={ this.renderMovie }
-    		style={ styles.listView }
-  		/>
-    );
+		return (
+			<ListView
+				dataSource={ this.state.dataSource }
+				renderRow={ this.renderMovie }
+				style={ styles.listView }
+			/>
+		);
 
-  }
+	}
 
 }
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center'
-  },
-  year: {
-    textAlign: 'center'
-  },
-  listView: {
-  	paddingTop: 20,
-  	backgroundColor: '#f5fcff'
-  }
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	thumbnail: {
+		width: 53,
+		height: 81
+	},
+	rightContainer: {
+		flex: 1,
+	},
+	title: {
+		fontSize: 20,
+		marginBottom: 8,
+		textAlign: 'center'
+	},
+	year: {
+		textAlign: 'center'
+	},
+	listView: {
+		paddingTop: 20,
+		backgroundColor: '#f5fcff'
+	}
 });
 
 AppRegistry.registerComponent('reactNativeOne', () => reactNativeOne);
